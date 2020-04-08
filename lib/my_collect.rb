a79eb01e543ec36243a6962cda3dd98881d83756
Jack Cooper <jackcooper1245@gmail.com>
2020-04-08 10:27:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(empty_array)
   i = 0
   new_collection = []
   while i < empty_array.length
-  new_collection << yield empty_array[i]
+  new_collection << yield(empty_array[i])
   i += 1
 end
 new_collection
